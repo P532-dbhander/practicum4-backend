653667be639bcd746cd97390f13fd7da853facc4
dbhander <dbhander@iu.edu>
2025-02-27 16:04:16 -0500

    small changes

--- src/main/java/edu/iu/habahram/ducksservice/controllers/DuckController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/DuckController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin
 @RequestMapping("/ducks")
 public class DuckController {
 
